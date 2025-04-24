314fae52517f9a8307f3d6556152b043353b323b
vlad.kragel <vlad.kragel.main@gmail.com>
2025-04-24 22:37:52 +0300

    feat: finish CLI subtasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,25 @@
 const parseArgs = () => {
-    // Write your code here 
+  console.log("Received request to parse command line arguments.");
+
+  const args = process.argv.slice(2);
+
+  const result = [];
+
+  for (let i = 0; i < args.length; i++) {
+    const current = args[i];
+
+    if (!current.startsWith("--")) return;
+
+    const argument = current.slice(2);
+    const number = args[i + 1];
+
+    result.push(`${argument} is ${number}`);
+    i++;
+  }
+
+  console.log("Parsed command line arguments: ", result.join(", "));
+
+  console.log("Environment variables were parsed successfully.");
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  console.log("Received request to parse environment variables.");
+
+  const result = Object.entries(process.env)
+    .filter(([key]) => key.startsWith("RSS_"))
+    .map(([key, value]) => `${key}=${value}`)
+    .join("; ");
+
+  console.log("Parsed environment variables: ", result);
+
+  console.log("Environment variables were parsed successfully.");
 };
 
 parseEnv();
