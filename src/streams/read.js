d7421819f0e4cdbe7484c62990cd84c26c06f037
vlad.kragel <vlad.kragel.main@gmail.com>
2025-04-25 23:48:11 +0300

    feat: finish stream subtasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import { createReadStream } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const SOURCE = "files";
+const FILE_NAME = "fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  console.log("Received request to read the file using readable stream.");
+
+  const filePath = join(__dirname, SOURCE, FILE_NAME);
+
+  console.log("Generated file path: ", filePath);
+
+  const readStream = createReadStream(filePath, "utf-8");
+
+  readStream.on("data", (data) => {
+    console.log(data);
+  });
+
+  readStream.on("error", (err) => {
+    console.error("Error reading file: ", err);
+  });
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import { Transform } from "stream";
+
 const transform = async () => {
-    // Write your code here 
+  const reverseFn = new Transform({
+    transform(chunk, encoding, callback) {
+      const input = chunk.toString().trim();
+
+      const reversed = input.split("").reverse().join("");
+
+      callback(null, reversed);
+    },
+  });
+
+  console.log("Please, type your text below.");
+
+  process.stdin.pipe(reverseFn).pipe(process.stdout);
 };
 
 await transform();
