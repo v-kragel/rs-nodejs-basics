03b3a9391fe6bb8f05531dc956af8d13737fb5ec
vlad.kragel <vlad.kragel.main@gmail.com>
2025-04-27 23:51:23 +0300

    feat: finish worker threads subtasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const WORKER_FILE = "worker.js";
+const workerFile = join(__dirname, WORKER_FILE);
+
+const START_NUMBER = 10;
+
+const CORES_COUNT = cpus().length;
+
+const getPromiseWorker = (number) =>
+  new Promise((resolve) => {
+    const worker = new Worker(workerFile, { workerData: number });
+
+    worker.on("message", (data) => resolve({ status: "resolved", data }));
+
+    worker.on("error", () => resolve({ status: "error", data: null }));
+  });
+
 const performCalculations = async () => {
-    // Write your code here
+  console.log("Received request to perform calculations.");
+
+  console.log("Cores count: ", CORES_COUNT);
+
+  const workers = Array.from({ length: CORES_COUNT }, (_, index) =>
+    getPromiseWorker(START_NUMBER + index)
+  );
+
+  const result = await Promise.all(workers);
+
+  console.log(result);
 };
 
 await performCalculations();
