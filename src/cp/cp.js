0412ce5e92620ae2d9c69a9772c636fea1cd565a
vlad.kragel <vlad.kragel.main@gmail.com>
2025-04-28 15:04:14 +0300

    feat: finish child process subtasks

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { spawn } from "child_process";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const SOURCE = "files";
+const SCRIPT_FILE = "script.js";
+const scriptFile = join(__dirname, SOURCE, SCRIPT_FILE);
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const child = spawn("node", [scriptFile, ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  process.stdin.pipe(child.stdin);
+  child.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
